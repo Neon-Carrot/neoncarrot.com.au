cb47b9b1068b5ab728a76eb2217442e80bdf994f
Caleb Ellis <caleb@neoncarrot.com.au>
2024-02-06 18:08:52 +1000

    Update images to match brand styles

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+export const COLORS = {
+  cardinal: "#cd113b",
+  dark: "#242430",
+  focus: "#2e96ff",
+  indigo: "#52006a",
+  light: "#ffffff",
+  maize: "#fff266",
+  orange: "#ff7600",
+  orangeAlt: "#ffa900",
+};
