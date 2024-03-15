d3d582c1815f43cac91bc5a2f0e4a6572a6c89db
Caleb Ellis <caleb@neoncarrot.com.au>
2024-03-16 01:43:47 +1000

    Add blog to website

--- src/constants.ts
+++ src/constants.ts
@@ -9,7 +9,8 @@ export const COLORS = {
   orangeLight: "#ffa900",
   purple: "#52006a",
   purpleDesat: "#46075a",
-  purpleLight: "#940fbd",
+  purpleMid: "#940fbd",
+  purpleLight: "#beb8f9",
   red: "#d1002d",
   redDesat: "#b80f33",
   redLight: "#f4718f",
@@ -17,4 +18,10 @@ export const COLORS = {
   yellow: "#ffea00",
   yellowDesat: "#f9ee76",
   yellowLight: "#fff7a3",
+} as const;
+
+export const ARTICLE_TOPICS = {
+  howToGuides: "How-to guides",
+  techTrends: "Tech trends",
+  misc: "Miscellaneous",
 } as const;
